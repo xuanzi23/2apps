f9a03d79e9007222d75cec27ccef8d22e33519c2
xuanzi <lawlierxuanzi@gmail.com>
2019-10-11 14:33:42 +0800

    Initial Commit

---
+++ 2Apps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2Apps
+//
+//  Created by YAP HAO XUAN on 11/10/2019.
+//  Copyright © 2019 YAP HAO XUAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2AppsTests/_AppsTests.swift
@@ -0,0 +1,34 @@
+//
+//  _AppsTests.swift
+//  2AppsTests
+//
+//  Created by YAP HAO XUAN on 11/10/2019.
+//  Copyright © 2019 YAP HAO XUAN. All rights reserved.
+//
+
+import XCTest
+@testable import _Apps
+
+class _AppsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
